209b96826486b7e8cfdec71809fa27e047f7be60
Joe Cserko <j.cserko@gmail.com>
2021-04-09 12:40:11 +0200

    Feat: dom spec.

--- task02-dom/__test__/dom.test.spec.js
+++ task02-dom/__test__/dom.test.spec.js
@@ -1,3 +1,26 @@
 describe('Dom manipuláció tesztelése', () => {
-    //
+    const template = `
+    <nav id="link-list">
+        <a href="https://training360.com/">Training360-link</a>
+        <a href="https://preply.com/">Preply-link</a>
+        <a href="https://www.w3schools.com/">W3-link</a>
+        <a href="https://fontawesome.com/">FontAwesome-link</a>
+        <a href="https://ngrx.io/">ngrx-link</a>
+    </nav>
+    `;
+
+    beforeEach(() => {
+        document.body.innerHTML = template;
+    });
+
+    test('Title attribútum vizsgálata', () => {
+        const { changeOuterLinks } = require('../dom');
+        changeOuterLinks();
+        const aElements = document.querySelectorAll(`nav#link-list a`);
+        expect(aElements[0].getAttribute('title')).toBe('Training360-link');
+        expect(aElements[1].getAttribute('title')).toBe('Preply-link');
+        expect(aElements[2].getAttribute('title')).toBe('W3-link');
+        expect(aElements[3].getAttribute('title')).toBe('FontAwesome-link');
+        expect(aElements[4].getAttribute('title')).toBe('ngrx-link');
+    });
 });
--- task02-dom/dom.js
+++ task02-dom/dom.js
@@ -1,5 +1,12 @@
+// Feladat:
+// Járjuk be az oldalon található link-list azonosítójú nav elem a elemeit.
+// Olvassuk ki minden elem belső html tartalmát és állítsuk be minden elem 
+// title attribútum értékének.
 function changeOuterLinks() {
-    // 
+    const aElements = document.querySelectorAll(`nav#link-list a`);
+    for (let i = 0; i < aElements.length; i++) {
+        aElements[i].setAttribute('title', aElements[i].innerHTML);
+    }
 }
 
 
